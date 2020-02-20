ccdc4cb4853d53df45139cb223f97c53b6c509d0
Sophie Schauer <sophieschauer@INKAs-MacBook-Pro.local>
2020-02-20 12:03:32 +0100

    new status

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public string SceneName;
+
+    public string SceneNameBack;
+    
+    public void GoToScene()
+    {
+        Debug.Log(SceneName);
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void GoBackToScene()
+    {
+        Debug.Log(SceneNameBack);
+        SceneManager.LoadScene(SceneNameBack);
+    }
+}
